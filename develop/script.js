25e369175a1e2d5c516284a1804ac5847ab0bfa8
Jose Cotto <josecotto12@gmail.com>
2023-02-25 16:30:29 -0500

    Add containers for Questions, end game screen and high scores

---
+++ develop/script.js
@@ -0,0 +1,8 @@
+//page start elements
+const timerEl = document.getElementById('timer');
+const startButtonEl = document.getElementById('start-game');
+const viewHighScoreEl = document.getElementById('view-high-score');
+
+//Add event listener that triggers the time, and renders the questions page while also hiding the page-start-container
+
+
