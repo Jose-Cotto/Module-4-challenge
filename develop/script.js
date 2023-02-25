f92372b4ba8e2a8a301ca232ac599c8c8fe62ec7
Jose Cotto <josecotto12@gmail.com>
2023-02-25 17:38:52 -0500

    Create timer and render question functions

--- develop/script.js
+++ develop/script.js
@@ -2,9 +2,11 @@
 const timerEl = document.getElementById('timer');
 const startButtonEl = document.getElementById('start-game');
 const viewHighScoreEl = document.getElementById('view-high-score');
-
-
-
+const pageStartContainerEl = document.getElementById('page-start-container');
+const questionContainerEl = document.getElementById('question-container');
+const endContainerEl = document.getElementById('end-container');
+const highScoreContainerEl = document.getElementById('high-score-container');
+var currentQuestion = 0;
 var questions = [
     {
         question:'Which of the following keywords is used to define a variable in Javascript?',
@@ -29,4 +31,28 @@ var questions = [
 ]
 //Add event listener that triggers the time, and renders the questions page while also hiding the page-start-container
 
+var startGame = function() {
+    pageStartContainerEl.classList.add('hide')
+    pageStartContainerEl.classList.remove('show');
+    questionContainerEl.classList.add('Show');
+    questionContainerEl.classList.remove('hide');
+    setTime();
+}
 
+
+var setTime = function() {
+    timeLeft = 40;
+
+    var timerDisplay = setInterval(function() {
+        timerEl.textContent = timeLeft;
+        timeLeft --;
+
+        if(timeLeft <= 0 || currentQuestion >= questions.length) {
+            clearInterval(setTime)
+            timerEl.textContent= 0;
+        }
+
+    },1000)
+}
+
+startButtonEl.addEventListener('click',startGame)
